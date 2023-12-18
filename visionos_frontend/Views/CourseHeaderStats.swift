b63bc3b4753e60143a0fdb8bf5d5d1d16e6b3b62
Abir Pal <abirpal282002@gmail.com>
2023-12-18 15:31:12 +0530

    Add Logo image and Contents.json file needed for the app's asset. This is necessary for displaying the logo image in the app.

--- visionos_frontend/Views/CourseHeaderStats.swift
+++ visionos_frontend/Views/CourseHeaderStats.swift
@@ -8,42 +8,64 @@
 import SwiftUI
 
 struct CourseHeaderStats: View {
+    @State private var search: String = ""
     var body: some View {
-        HStack(spacing:15){
-//            Live or not
-            HStack {
-                Circle()
-                    .fill(.systemRed)
-                    .frame(width: 12)
-                Text("Live Now")
-                    .fontWeight(.bold)
-                    .font(.system(size: 18))
+        VStack {
+            HStack(){
+                Image(.logo)
+                Text("Virtual Class")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                HStack {
+                    HStack {
+                        Image(systemName: "mic.fill")
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                        SecureField("Password", text: $search)
+                            .frame(height: 6)
+                            .padding()
+                        
+                            .cornerRadius(10.0)
+                    }
+                }
+                
+                
             }
-            .padding(.horizontal, 8)
-            .padding(.vertical, 5)
-            .glassBackgroundEffect()
-            
-//            No Of Watching
-            HStack {
-                Image(systemName: "eye")
-                Text("22k Watching")
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
+            HStack(spacing:15){
+                //            Live or not
+                HStack {
+                    Circle()
+                        .fill(.systemRed)
+                        .frame(width: 12)
+                    Text("Live Now")
+                        .fontWeight(.bold)
+                        .font(.system(size: 18))
+                }
+                .padding(.horizontal, 8)
+                .padding(.vertical, 5)
+                .glassBackgroundEffect()
+                
+                //            No Of Watching
+                HStack {
+                    Image(systemName: "eye")
+                    Text("22k Watching")
+                        .fontWeight(.semibold)
+                        .font(.subheadline)
+                }
+                .padding(.horizontal, 10)
+                .padding(.vertical, 5)
+                .glassBackgroundEffect()
+                
+                Spacer()
+                //            Location
+                HStack{
+                    Image(systemName: "mappin.and.ellipse")
+                    Text("Florida, USA")
+                }
+                
             }
-            .padding(.horizontal, 10)
-            .padding(.vertical, 5)
-            .glassBackgroundEffect()
-            
-            Spacer()
-//            Location
-            HStack{
-                Image(systemName: "mappin.and.ellipse")
-                Text("Florida, USA")
-            }
-            
         }
         .padding(.horizontal, 30)
-        .padding(.vertical, 10)
+        .padding(.top, 20)
     }
 }
 
